f01d028414fc853301547b671ab06a1f44bac03b
Cosmin Lupu <cosmin.lupu90@gmail.com>
2020-02-11 09:31:17 +0100

    Added logging of blocked scripts

--- src/checks.js
+++ src/checks.js
@@ -1,13 +1,22 @@
 import { patterns, TYPE_ATTRIBUTE } from './variables'
+const loggedBlacklist = [];
 
-export const isOnBlacklist = (src, type) => (
-    src &&
+export const isOnBlacklist = (src, type) => {
+    const onBlacklist = src &&
     (!type || type !== TYPE_ATTRIBUTE) &&
     (
         (!patterns.blacklist || patterns.blacklist.some(pattern => pattern.test(src))) &&
         (!patterns.whitelist || patterns.whitelist.every(pattern => !pattern.test(src)))
-    )
-)
+    );
+
+
+    if(onBlacklist && !loggedBlacklist.includes(src)){
+        loggedBlacklist.push(src);
+        console.info(`Script src '${src}' is blocked`);
+    }
+
+    return onBlacklist;
+}
 
 export const willBeUnblocked = function(script) {
     const src = script.getAttribute('src')
